7f6c0938a237e0c68d2b53d87df2d06205109415
BehzodHamroyev <behzodhamroyev007@gmail.com>
2023-09-18 09:56:05 +0500

    firts commit

--- src/pages/about/About.tsx
+++ src/pages/about/About.tsx
@@ -1,5 +1,12 @@
+import { AboutWrapper } from "./AboutStyles";
+
 const About = () => {
-  return <div>About</div>;
+  return <AboutWrapper>
+
+
+
+    
+  </AboutWrapper>;
 };
 
 export default About;
