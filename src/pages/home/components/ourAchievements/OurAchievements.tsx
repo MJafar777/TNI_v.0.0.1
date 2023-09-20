92cf80a99dfc15fc1e4e296817e548fdc4a432c6
BehzodHamroyev <behzodhamroyev007@gmail.com>
2023-09-20 07:23:54 +0500

    section of  achivements

--- src/pages/home/components/ourAchievements/OurAchievements.tsx
+++ src/pages/home/components/ourAchievements/OurAchievements.tsx
@@ -1,3 +1,4 @@
+import { AchivementImg1, AchivementImg2, AchivementImg3, AchivementImg4 } from "../../../../assets/images/components/achivements";
 import {
   Title,
   TopContentOfDelivery,
@@ -14,7 +15,6 @@ import {
 } from "./OurAchievementsStyles";
 
 const OurAchievements = () => {
-
   return (
     <OurAchievementsWrapper>
       <TopContentOfDelivery>
@@ -27,9 +27,43 @@ const OurAchievements = () => {
 
       <AchievementsContainer>
         <EachAchievement>
-          <EachAchievementsCardImg></EachAchievementsCardImg>
-          <EachAchievementsCardTitle></EachAchievementsCardTitle>
-          <EachAchievementsCardContent></EachAchievementsCardContent>
+          <EachAchievementsCardImg
+            src={AchivementImg1}
+          ></EachAchievementsCardImg>
+          <EachAchievementsCardTitle>15+</EachAchievementsCardTitle>
+          <EachAchievementsCardContent>
+            TYPES OF PRODUCTS
+          </EachAchievementsCardContent>
+        </EachAchievement>
+
+        <EachAchievement>
+          <EachAchievementsCardImg
+            src={AchivementImg2}
+          ></EachAchievementsCardImg>
+          <EachAchievementsCardTitle>50+</EachAchievementsCardTitle>
+          <EachAchievementsCardContent>
+            LOCAL CUSTOMERS{" "}
+          </EachAchievementsCardContent>
+        </EachAchievement>
+
+        <EachAchievement>
+          <EachAchievementsCardImg
+            src={AchivementImg3}
+          ></EachAchievementsCardImg>
+          <EachAchievementsCardTitle>5+</EachAchievementsCardTitle>
+          <EachAchievementsCardContent>
+            COUNTRIES WE IMPORT FROM{" "}
+          </EachAchievementsCardContent>
+        </EachAchievement>
+
+        <EachAchievement>
+          <EachAchievementsCardImg
+            src={AchivementImg4}
+          ></EachAchievementsCardImg>
+          <EachAchievementsCardTitle>10+</EachAchievementsCardTitle>
+          <EachAchievementsCardContent>
+            INTERNATIONAL PARTNERS{" "}
+          </EachAchievementsCardContent>
         </EachAchievement>
       </AchievementsContainer>
     </OurAchievementsWrapper>
--- src/pages/home/components/ourAchievements/OurAchievementsStyles.ts
+++ src/pages/home/components/ourAchievements/OurAchievementsStyles.ts
@@ -6,19 +6,50 @@ export const OurAchievementsWrapper = styled.div`
   margin-top: 300px;
   display: flex;
   justify-content: center;
+  flex-direction: column;
+  align-items: center;
 `;
 
 export const AchievementsContainer = styled.div`
   width: 100%;
+  padding: 40px 0;
   display: flex;
   justify-content: space-evenly;
   align-items: center;
   flex-wrap: wrap;
+  gap: 90px;
   background: linear-gradient(180deg, #020918 0%, #252b42 97.56%);
-  height: 355px;
+  margin-top: 10px;
 `;
 
-export const EachAchievement = styled.div``;
-export const EachAchievementsCardImg = styled.div``;
-export const EachAchievementsCardTitle = styled.div``;
-export const EachAchievementsCardContent = styled.div``;
+export const EachAchievement = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 14px;
+`;
+
+export const EachAchievementsCardImg = styled.img``;
+
+export const EachAchievementsCardTitle = styled.div`
+  color: #fff;
+  font-family: Montserrat;
+  font-size: 36px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+  text-align: center;
+  padding: 10px;
+  border-bottom: 2px white solid;
+`;
+
+export const EachAchievementsCardContent = styled.div`
+  color: #fff;
+  font-family: Mazzard;
+  font-size: 18px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 168.2%; /* 30.276px */
+  letter-spacing: 1.26px;
+  text-align: center;
+`;
