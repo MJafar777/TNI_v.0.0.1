f4ee43e3d9eca08c26251289d1ee78cd25571947
MJafar777 <mirzaraximovjafar2001@gmail.com>
2023-09-21 10:21:29 +0500

    create new review section

---
+++ src/pages/products/pages/reviews/ui/ReviewProduct.tsx
@@ -0,0 +1,9 @@
+import { FC } from "react";
+
+import { ReviewProductWrapper } from "./reviewProductStyle";
+
+const ReviewProduct: FC = () => {
+  return <ReviewProductWrapper>ReviewProduct</ReviewProductWrapper>;
+};
+
+export default ReviewProduct;
